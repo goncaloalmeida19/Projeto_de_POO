7df0b4746d22bfb99b714befafce9a607b4f7333
joaobjsantos <93948049+joaobjsantos@users.noreply.github.com>
2021-11-16 16:52:58 +0000

    addCarrinho e removeCarrinho + contains

--- src/projeto/Venda.java
+++ src/projeto/Venda.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class Venda {
     private final Data data;
-    private List<Item> carrinho;
+    private final List<Item> carrinho;
 
     public Venda(Data data) {
         this.data = data;
@@ -20,23 +20,31 @@ public class Venda {
     public List<Item> getCarrinho() {
         return carrinho;
     }
-
-    public void addCarrinho(Produto produto) {
-        boolean in = false;
+    
+    private Item contemProduto(Produto p){
         for(Item item: carrinho){
-            if(item.getProduto().equals(produto)){
-                item.incrementarQuantidade();
-                in = true;
-                break;
+            if(item.getProduto().igual(p)){
+                return item;
             }
         }
-        if(!in){
+        return null;
+    }
+    
+    public void addCarrinho(Produto produto) {
+        Item i = contemProduto(produto);
+        if(i != null)
+            i.incrementarQuantidade();
+        else{
             carrinho.add(new Item(produto, 1));
         }
     }
 
-    public void removeCarrinho(Produto produto) {
-
+    public boolean removeCarrinho(Produto produto) {
+        Item i = contemProduto(produto);
+        if(i != null){
+            carrinho.remove(i);
+            return true;
+        }else return false;
     }
 
     public double precoFinal(){
