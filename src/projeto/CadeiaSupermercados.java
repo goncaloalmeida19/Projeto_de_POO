3f448b286e54411888df28b7eab18ef16e76e1b5
Gonçalo Almeida <gamertota4@gmail.com>
2021-11-17 11:04:54 +0000

    Alterações em ...
    - Interface de login feita (se quiseres uma melhor altera)
    
    Funções criadas:
     - readIntProtection -> Ler int com proteção
     - readEmail -> Ler email com proteção e devolve null caso não possua '@'
    - contemEmail -> Devolve o cliente que possui o email passado como parâmetro ou null se não existir nenhum cliente que possua esse email

--- src/projeto/CadeiaSupermercados.java
+++ src/projeto/CadeiaSupermercados.java
@@ -1,4 +1,38 @@
 package projeto;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class CadeiaSupermercados {
+
+    List<Cliente> clientes = new ArrayList<>();
+
+    // Método que lê um inteiro e devolve-o sem erros, se um acontecer devolve -1 (sendo considerado erro neste programa)
+    public int readIntProtection(Scanner scanner){
+        if(scanner.hasNextInt()) return scanner.nextInt();
+        else{
+            scanner.nextLine();
+            return -1;
+        }
+    }
+
+    public String readEmail(Scanner scanner){
+        scanner.next();
+        String email;
+        do{
+            email = scanner.nextLine();
+        }while(email.isEmpty());
+        if(email.indexOf('@') != -1) return email;
+        return null;
+    }
+
+    public Cliente contemEmail(String email){
+        for(Cliente c: clientes){
+            if(c.getEmail().equals(email)){
+                return c;
+            }
+        }
+        return null;
+    }
 }
